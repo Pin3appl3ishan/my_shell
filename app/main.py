bc222fbc7606fa3edac30404de27c3cb52615b79
Pin3appl3ishan <sthaishan2004@gmail.com>
2026-02-15 19:50:14 +0545

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 
+BUILTINS = ["echo", "exit", "type"]
 
 def main():
     while True:
@@ -15,6 +16,15 @@ def main():
 
         if (cmd_name == "exit"):
             break
+        elif cmd_name == "type":
+            if len(args) != 1:
+                print("Usage: type <command>")
+                continue
+            target = args[0]
+            if target in BUILTINS:
+                print(f"{target} is a shell builtin")
+            else:
+                print(f"{target}: not found")
         elif cmd_name == "echo":
             print(" ".join(args))
         else:
